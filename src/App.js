4405e69fe6551ce56ec0917fbbfdb2288e982598
Dakota Kim <codingvampire@gmail.com>
2022-05-08 19:05:43 -0400

    try selecotr

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
-import { useRecoilState, atom } from 'recoil';
+import { useRecoilState, useRecoilValue, atom, selector } from 'recoil';
 
 function App() {
   return (
     <div className="App">
       <TextInput />
+      <CharacterCount />
     </div>
   );
 }
 
-const textState = atom({key: 'textState', default: ''});
+const textState = atom({ key: 'textState', default: '' });
+
+const charCountState = selector({
+  key: 'charCountState', get: ({ get }) => {
+    const text = get(textState);
+    return text.length;
+  }
+});
 
 function TextInput() {
   const [text, setText] = useRecoilState(textState);
@@ -24,6 +32,16 @@ function TextInput() {
       <br />
       Echo: {text}
     </div>
+  );
+}
+
+function CharacterCount() {
+  const count = useRecoilValue(charCountState);
+
+  return (
+    <div>
+      Character count: {count}
+    </div> 
   );
 }
 
